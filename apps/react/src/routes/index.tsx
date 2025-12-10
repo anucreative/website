d3f07e8ba6faca61fe85bfb61cc9985216eb7f4c
Robert Douglas <rob@anucreative.com>
2025-12-10 20:31:05 +0100

    chore(react): add avatar to button

--- apps/react/src/routes/index.tsx
+++ apps/react/src/routes/index.tsx
@@ -1,6 +1,8 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { Logo } from 'src/components/Logo'
 
+import favicon from '/favicon.png'
+
 export const Route = createFileRoute('/')({
   component: Home,
 })
@@ -14,7 +16,7 @@ export function Home() {
       <h1>anu creative</h1>
       <p>design and development • web and mobile</p>
       <a href="/cv" className="button">
-        View CV
+        View CV <img src={favicon} alt="Photo of Robert Douglas" width={25} height={25} />
       </a>
     </div>
   )
