0ed8631daf380167c993e575556b4e852c362ae9
Robert Douglas <rob@anucreative.com>
2025-12-10 14:49:24 +0100

    chore(react): extract logo svg into component

--- apps/react/src/routes/index.tsx
+++ apps/react/src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { Logo } from 'src/components/Logo'
 
 export const Route = createFileRoute('/')({
   component: Home,
@@ -7,18 +8,8 @@ export const Route = createFileRoute('/')({
 export function Home() {
   return (
     <div className="splash">
-      <div className="svg-wrapper">
-        <svg viewBox="-30 -30 373 165" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <title>anu creative logo</title>
-          <path
-            className="anucreative"
-            d="M53 0.507948C35.5 0.174615 0.5 10.208 0.5 53.0079C0.5 95.8079 35.5 104.841 53 104.008C70 104.508 104 95.0079 104 53.0079C104 11.0079 139.667 0.507948 157.5 0.507948C175 1.00795 210 12.208 210 53.0079C210 93.8079 245 104.008 262.5 104.008C279 104.508 312 95.0079 312 53.0079V0.507948"
-            stroke="white"
-            strokeWidth="40"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
+      <div className="logo-wrapper">
+        <Logo />
       </div>
       <h1>anu creative</h1>
       <p>design and development • web and mobile</p>
